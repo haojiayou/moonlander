a7607010dca8ab38fbe9510c4173867493f743f9
haojiayou <84018724+haojiayou@users.noreply.github.com>
2021-09-29 21:44:40 +0900

    01
    01

---
+++ moon_lander/src/moon_lander/obstacles.java
@@ -0,0 +1,39 @@
+package moon_lander;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.imageio.ImageIO;
+
+public class obstacles {
+	/**
+     * X coordinate of the obstracle.
+     */
+    public int x;
+    /**
+     * Y coordinate of the obstracle.
+     */
+    public int y;
+    
+    private BufferedImage obstracleImg;
+    
+    
+    /**
+     * Width of obstracle.
+     */
+    public int obstacleImgWidth;
+    /**
+     * Height of obstracle.
+     */
+    public int obstacleImgHeight;
+    
+    public void obstacle()
+    {
+        
+    }
+}
